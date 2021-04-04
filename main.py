d98a72d767a86501838319a03f285882b461877f
jkzayah <isaiah.gordon.developer@gmail.com>
2021-04-04 02:22:56 -0300

    Fixed current time bug in main.py

--- main.py
+++ main.py
@@ -30,6 +30,8 @@ initialize_frontend()
 
 while True:
 
+    current_time = datetime.utcnow()
+
     # If it's the middle of the night: go idle for 4 hours.
     if current_time > current_time.replace(hour=2) or current_time < current_time.replace(hour=4):
         gameMaster.transition(
